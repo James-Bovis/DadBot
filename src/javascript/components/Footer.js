4d83c68ad3a2c39bcfc660c846c5fc57a2377f30
James Bovis <hello@jamesbovis.com>
2020-02-02 12:55:44 +0000

    DadBot 2.0
    This commit contains the all-new source files for DadBot 2.0. DadBot has been
    completely re-written from the ground up and is now built using React and
    `create-react-app` which allows the maintaining and developing of DadBot to be
    easier than ever.
    
    DadBot 2.0 now also comes the following improvements in it's source code and
    visuals:
    - Support for more pleasing UI when loading a new random joke
    - Update iPhone mock-up showing Facebook Messenger
    - A better designed mobile experience
    - A much lower CSS and JS bundle size due in part to code splitting and a leaner
    stack
    
    DadBot 2.0 also represents the phenomenal growth I have personally achieved as a
    developer since releasing DadBot 1.0 in 2017. Hopefully, this growth will
    continue to show in more improvements to DadBot in the very near future.

---
+++ src/javascript/components/Footer.js
@@ -0,0 +1,37 @@
+// @flow
+
+import * as React from 'react'
+
+const footer = (): React.Element<'footer'> => (
+  <footer>
+    <p>
+      {`Built with love by `}
+      <a
+        href='https://jamesbovis.com'
+        rel='noopener noreferrer'
+        target='_blank'
+      >
+        {'James Bovis'}
+      </a>
+      {` | DadBot is built using `}
+      <a href='https://rundexter.com' rel='noopener noreferrer' target='_blank'>
+        {'DEXTER'}
+      </a>
+      {` Dadbot uses an API to deliver jokes to users. Thanks to `}
+      <a
+        href='http://icanhazdadjoke.com'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        {`icanhazdadjoke.com `}
+      </a>
+      {`for providing the information. Thanks to `}
+      <a href='https://giphy.com' rel='noopener noreferrer' target='_blank'>
+        {`Giphy`}
+      </a>
+      {` and their awesome API for giving DadBot his amazing GIF replies.`}
+    </p>
+  </footer>
+)
+
+export default footer
