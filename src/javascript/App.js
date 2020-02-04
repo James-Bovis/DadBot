c555471ab392cde982f141ed4d729755a146e937
James Bovis <hello@jamesbovis.com>
2020-02-04 20:27:19 +0000

    Reorganise file structure and directory

--- src/javascript/App.js
+++ src/javascript/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.sass'
+import logo from '../assets/logo.svg'
+import '../stylesheets/App.sass'
 
 function App() {
   return (
